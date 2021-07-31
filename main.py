1aa3f9a4b073eaf3cc29d0fac8d4e42131a6672a
FaizAlam <mohdfaizalam53@gmail.com>
2021-07-31 22:15:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pync import Notifier
+from datetime import datetime
+import emoji
+
+def grab_data():
+    url = "https://coinmarketcap.com/currencies/bitcoin/"
+    r = requests.get(url = url)
+    soup = BeautifulSoup(r.text)
+    price = soup.find('div',{"class":"priceValue___11gHJ"})
+    perchange = soup.find('span',{"class":"gEePkg"})
+    pos = perchange.find('span',{"class":"icon-Caret-up"})
+    neg = perchange.find('span',{"class":"icon-Caret-down"})
+    if(pos):
+      change = '+'+perchange.text
+    elif(neg):
+       change = '-'+perchange.text
+  
+    #printing
+    print("BITCOIN")
+    print("Price :",price.text) 
+    print("Change :",change)
+
+    #toast
+    msg = f'price : {price.text}, change % : {change}'
+    Notifier.notify(msg,title="Bitcoin \U0001F4B9")
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(grab_data,'interval', seconds=60)
+scheduler.start()
